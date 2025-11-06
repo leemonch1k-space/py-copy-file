89fb815205ad5c46ab5b2d7170f01788a28c4d84
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-11-06 16:54:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+    if len(command_list) < 3 or command_list[0] != "cp":
+        return
+
+    try:
+        with open(command_list[1], mode="r") as file_read:
+            file_data = file_read.read()
+
+        with open(command_list[2], mode="w") as file_write:
+            file_write.write(file_data)
+    except FileNotFoundError:
+        return
